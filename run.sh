6971cb69ca4e9a4a24a588450a884ca5a63f3b22
Alison Swearingen <alsw5948@fiji-2.colorado.edu>
2020-05-12 14:16:13 -0600

    adding .gitignore, run.sh, config file

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -p long
+#SBATCH --job-name=JB_K-M_rnaseq
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=alsw5948@colorado.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=6gb
+#SBATCH --time=50:00:00
+#SBATCH --output=nextflow.out
+#SBATCH --error=nextflow.err
+
+pwd; hostname; date
+echo "Hi there -- you've requested $SLURM_CPUS_ON_NODE core."
+
+module load singularity/3.1.1
+
+nextflow run nf-core/rnaseq -r 1.4.2 \
+-profile singularity \
+--reads '/scratch/Users/alsw5948/JB_K-M/JB_K-M_rnaseq_analysis/fastq/*_{1,2}.fastq.gz' \ 
+--fasta /scratch/Users/alsw5948/JB_K-M/JB_K-M_rnaseq_analysis/JB_K-M_rnaseq/util/NCBIM37.genome.fa \
+--gtf /scratch/Users/alsw5948/JB_K-M/JB_K-M_rnaseq_analysis/JB_K-M_rnaseq/util/gencode.vM1.annotation.gtf \
+--email alsw5948@colorado.edu \
+--email_on_fail alsw5948@colorado.edu
+-resume \
+-c nextflow.config
+
+date
