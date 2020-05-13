84cb6706d05e976b1c2371fc74f18e20d29cfa32
Alison Swearingen <alsw5948@fiji-1.colorado.edu>
2020-05-13 10:02:02 -0600

    finally the run.sh that works

--- run.sh
+++ run.sh
@@ -17,10 +17,12 @@ module load singularity/3.1.1
 
 nextflow run nf-core/rnaseq -r 1.4.2 \
 -profile singularity \
---reads '/scratch/Users/alsw5948/JB_K-M/JB_K-M_rnaseq_analysis/fastq/*_{1,2}.fastq.gz' \ 
---genome mm10
+--reads "/scratch/Users/alsw5948/JB_K-M/JB_K-M_rnaseq_analysis/fastq/*_{1,2}.fastq.gz" \
+--fasta /scratch/Users/alsw5948/JB_K-M/JB_K-M_rnaseq_analysis/JB_K-M_rnaseq/util/NCBIM37.genome.fa \
+--gtf /scratch/Users/alsw5948/JB_K-M/JB_K-M_rnaseq_analysis/JB_K-M_rnaseq/util/gencode.vM1.annotation.gtf \
+--gencode \
 --email alsw5948@colorado.edu \
---email_on_fail alsw5948@colorado.edu
+--email_on_fail alsw5948@colorado.edu \
 -resume \
 -c nextflow.config
 
